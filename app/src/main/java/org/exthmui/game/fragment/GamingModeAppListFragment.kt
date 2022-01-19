456ed292235928865f450bd531dc8793592ec768
Pranav Vashi <neobuddy89@gmail.com>
2022-01-19 22:42:50 +0530

    Adapt for Syberia
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ia48212382e23610fb3bec80633ea843a6d796eca

--- app/src/main/java/org/exthmui/game/fragment/GamingModeAppListFragment.kt
+++ app/src/main/java/org/exthmui/game/fragment/GamingModeAppListFragment.kt
@@ -18,7 +18,7 @@ package org.exthmui.game.fragment
 
 import android.provider.Settings
 import androidx.preference.PreferenceDataStore
-import com.krypton.settings.preference.SystemSettingsStore
+import com.syberia.settings.preference.SystemSettingsStore
 
 import org.exthmui.game.R
 
